86d573a42b04206a3de4c106b22b74accb26db2b
Jason Kirchman <kirchman.jason@icloud.com>
2023-04-19 14:35:18 -0500

    Get current weather from api and store in App state

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import Header from './components/Header/Header';
 import CurrentWeather from './components/CurrentWeather/CurrentWeather';
-import {getForecast} from './api';
+import getWeather from './api';
 import './App.css';
 import ConditionsSelector from './components/ConditionsSelector/ConditionsSelector';
 import DailyForecast from './components/DailyForecast/DailyForecast';
+import { useEffect, useState } from 'react';
 
 function App() {
-  getForecast('67401', 3);
+  const [location, setLocation] = useState('67401');
+  const [weather, setWeather] = useState(null);
+  const [errorMsg, setError] = useState('');
+
+  const getData = async () => {
+    setWeather(await getWeather(location, 1, setError));
+  }
+
+  useEffect(() => {
+    getData();
+  }, [])
 
   return (
     <div className="App">
       <Header />
       <main>
-        <CurrentWeather />
+        {weather ? <CurrentWeather location={weather.location} current={weather.current}/> : <p>Loading</p>}
         <ConditionsSelector />
         <DailyForecast />
       </main>
--- src/components/CurrentWeather/CurrentWeather.js
+++ src/components/CurrentWeather/CurrentWeather.js
@@ -1,12 +1,16 @@
-import './CurrentWeather.css';
+import stateDict from '../../stateDict';
 import sunny from '../../assets/icons/sunny.svg';
+import './CurrentWeather.css';
 
-const CurrentWeather = () => {
+const CurrentWeather = ({location, current}) => {
+  
   return(
     <section className='current-weather-parent'>
       <div>
-        <h1 className='current-weather-location'>Denver, CO</h1>
-        <div className='current-weather-data'>Sunny - 72&#176; - 12mph</div>
+        <h1 className='current-weather-location'>{`${location.name}, ${stateDict[location.region]}`}</h1>
+        <div className='current-weather-data'>
+          {`${current.condition.text} - ${Math.round(current.temp_f)}\u00B0 - ${Math.round(current.wind_mph)}mph`}
+        </div>
       </div>
       <img src={sunny} alt={'current condition icon'} className='current-weather-icon'/>
     </section>
