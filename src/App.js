da8bcc6937bc2af8cd21a5767103d58cc084d900
Jason Kirchman <kirchman.jason@icloud.com>
2023-04-18 22:14:52 -0500

    Add Header componenet and basic elements

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import './App.css';
+import Header from './components/Header/Header';
 import {getForecast} from './api';
+import './App.css';
 
 function App() {
   getForecast('67401', 3);
 
   return (
     <div className="App">
+      <Header />
       <main></main>
     </div>
   );
