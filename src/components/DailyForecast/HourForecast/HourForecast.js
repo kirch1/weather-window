9563954edcd9629939806e623eb788554986f0a7
Jason Kirchman <kirchman.jason@icloud.com>
2023-04-20 11:30:40 -0500

    Add additional hourly data

--- src/components/DailyForecast/HourForecast/HourForecast.js
+++ src/components/DailyForecast/HourForecast/HourForecast.js
@@ -3,6 +3,7 @@ import wind from '../../../assets/icons/wind.svg';
 import rain from '../../../assets/icons/rain.svg';
 import snowflake from '../../../assets/icons/snowflake.svg';
 import arrow from '../../../assets/icons/arrow.svg';
+import humidity from '../../../assets/icons/humidity.svg';
 
 const HourForecast = ({hour}) => {
   const time = new Date(hour.time).toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
@@ -13,7 +14,10 @@ const HourForecast = ({hour}) => {
         <img className='hour-icon' src={hour.condition.icon} alt='Hour icon'/>
       </div>
       <div className='hour-forecast-row'>
-        <p className='hour-data'>{`${Math.round(hour.temp_f)}\u00B0`}</p>
+        <p className='hour-data'>{`Temp: ${Math.round(hour.temp_f)}\u00B0`}</p>
+        <p className='hour-data'>{`Feel: ${Math.round(hour.feelslike_f)}\u00B0`}</p>
+      </div>
+      <div className='hour-forecast-row'>
         <div className='hour-forecast-mini'>
           <img className='hour-icon-small' src={rain} alt='Hour icon small'/>
           <p className='hour-data'>{`${Math.round(hour.chance_of_rain)}%`}</p>
@@ -22,14 +26,19 @@ const HourForecast = ({hour}) => {
           <img className='hour-icon-small' src={snowflake} alt='Hour icon small'/>
           <p className='hour-data'>{`${Math.round(hour.chance_of_snow)}%`}</p>
         </div>
+        <div className='hour-forecast-mini'>
+          <img className='hour-icon-small' src={humidity} alt='Hour icon small'/>
+          <p className='hour-data'>{`${Math.round(hour.humidity)}%`}</p>
+        </div>
       </div>
       <div className='hour-forecast-row'>
         <div className='hour-forecast-mini'>
           <img className='hour-icon-small' src={wind} alt='Hour icon small'/>
-          <p className='hour-data'>{`${Math.round(hour.wind_mph)} mph`}</p>
+          <p className='hour-data'>{`${Math.round(hour.wind_mph)} / ${Math.round(hour.gust_mph)} mph`}</p>
           <img className='hour-icon-small arrow' src={arrow} alt='Arrow icon' style={{transform:`rotate(${hour.wind_degree}deg)`}}/>
         </div>
       </div>
+
     </div>
   )
 }
